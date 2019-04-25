939d916ce8f04ef2f9d180d4215dc6508fc6b8fa
gkarumba <gachegua@gmail.com>
2019-04-25 17:13:00 +0300

    Add response section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,19 @@ function checkTriangle(){
     let sideB = document.getElementById('side_b').value;
     let sideC = document.getElementById('side_c').value;
 
+    if(side_a===side_b && side_b===side_c){
+        let output = 'This is an equilateral triangle';
+        return document.getElementById('display_text').innerHTML = output;
+    }else if(side_a===side_b || side_b===side_c || side_c===side_a){
+        let output = 'This is an isocelles triangle'
+        return document.getElementById('display_text').innerHTML = output;
+    }else if(((side_a + side_b)>side_c) && ((side_b + side_c)> side_a) && ((side_c + side_a)>side_b)){
+        let output = 'This is a scalene triangle'
+        return document.getElementById('display_text').innerHTML = output;
+    }else{
+        let output = 'This is not a triangle';
+        return document.getElementById('display_text').innerHTML = output;
+    }
 
     // console.log(sideA);
     // console.log(sideB);
