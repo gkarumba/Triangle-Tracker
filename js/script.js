8acc095bf33cd472df3de2bd83473930a6f453d0
gkarumba <gachegua@gmail.com>
2019-04-27 19:10:52 +0300

    Add the JS functionality for checking type of triangle

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 const checkBtn = document.getElementById('submit');
 
 function checkTriangle(){
-    let sideA = document.getElementById('side_a').value;
-    let sideB = document.getElementById('side_b').value;
-    let sideC = document.getElementById('side_c').value;
+    let sideA = parseInt(document.getElementById('side_a').value);
+    let sideB = parseInt(document.getElementById('side_b').value);
+    let sideC = parseInt(document.getElementById('side_c').value);
 
-    if(side_a===side_b && side_b===side_c){
-        let output = 'This is an equilateral triangle';
-        return document.getElementById('display_text').innerHTML = output;
-    }else if(side_a===side_b || side_b===side_c || side_c===side_a){
-        let output = 'This is an isocelles triangle'
-        return document.getElementById('display_text').innerHTML = output;
-    }else if(((side_a + side_b)>side_c) && ((side_b + side_c)> side_a) && ((side_c + side_a)>side_b)){
-        let output = 'This is a scalene triangle'
-        return document.getElementById('display_text').innerHTML = output;
-    }else{
+
+    if (sideA + sideB >= sideC && sideB + sideC >= sideA && sideC + sideA >= sideB){
+        if(sideA === sideB && sideB === sideC){
+            let output = 'This is an equilateral triangle';
+            return document.getElementById('display_text').innerHTML = output;
+        }else if(sideA === sideB || sideB === sideC || sideC === sideA){
+            let output = 'This is an isocelles triangle'
+            return document.getElementById('display_text').innerHTML = output;
+        }else if((sideA !== sideB) && (sideB !== sideC) && (sideC !== sideA)){
+            let output = 'This is a scalene triangle'
+            return document.getElementById('display_text').innerHTML = output;
+        }
+    }
+    else{
         let output = 'This is not a triangle';
         return document.getElementById('display_text').innerHTML = output;
     }
-
     // console.log(sideA);
     // console.log(sideB);
     // console.log(sideC);
